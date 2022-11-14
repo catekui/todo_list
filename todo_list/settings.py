895450a60493326959f51d6880eea79a854bdbfc
catekui <catherine.wanjiku@moringaschool.com>
2022-11-14 13:30:35 +0300

    debug

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -104,10 +104,10 @@ WSGI_APPLICATION = 'todo_list.wsgi.application'
 #     }
 # }
 DATABASES = {
-    'default': dj_database_url.config(default=config('DATABASE_URL')
-
+    'default': dj_database_url.config(default=config('DATABASE_URL'))
 
 }
+
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
